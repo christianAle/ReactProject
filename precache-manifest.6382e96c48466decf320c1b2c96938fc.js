1ea0de711c8e1f45b397d3255cbcdbcec3fa59a8
christianAle <cristr97@gmail.com>
2019-05-31 23:56:52 -0600

    Updates

---
+++ precache-manifest.6382e96c48466decf320c1b2c96938fc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4affba3139b66e6526c0944efcc4a73",
+    "url": "/christianAle/ReactProject/index.html"
+  },
+  {
+    "revision": "4377e14055ba36a3ed5f",
+    "url": "/christianAle/ReactProject/static/css/2.9798096d.chunk.css"
+  },
+  {
+    "revision": "e0f6122e8fdc2a8214e7",
+    "url": "/christianAle/ReactProject/static/css/main.ba5ea692.chunk.css"
+  },
+  {
+    "revision": "4377e14055ba36a3ed5f",
+    "url": "/christianAle/ReactProject/static/js/2.876007cb.chunk.js"
+  },
+  {
+    "revision": "e0f6122e8fdc2a8214e7",
+    "url": "/christianAle/ReactProject/static/js/main.70784714.chunk.js"
+  },
+  {
+    "revision": "c2d2dcd2db570237e38b",
+    "url": "/christianAle/ReactProject/static/js/runtime~main.44f58ab0.js"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/christianAle/ReactProject/static/media/Montserrat-Bold.88932dad.ttf"
+  },
+  {
+    "revision": "7940efc40d8e3b477e16cc41b0287139",
+    "url": "/christianAle/ReactProject/static/media/Poppins-Bold.7940efc4.ttf"
+  },
+  {
+    "revision": "a4e11dda40531debd374e4c8b1dcc7f4",
+    "url": "/christianAle/ReactProject/static/media/Poppins-Medium.a4e11dda.ttf"
+  },
+  {
+    "revision": "731a28a413d642522667a2de8681ff35",
+    "url": "/christianAle/ReactProject/static/media/Poppins-Regular.731a28a4.ttf"
+  },
+  {
+    "revision": "e63b93dfac2600782654e2b87910d681",
+    "url": "/christianAle/ReactProject/static/media/Poppins-SemiBold.e63b93df.ttf"
+  },
+  {
+    "revision": "f179c038d6b325762b4e605c9ae6137a",
+    "url": "/christianAle/ReactProject/static/media/bg-head-02.f179c038.jpg"
+  },
+  {
+    "revision": "e11f134605d70114573fc63a872dac4c",
+    "url": "/christianAle/ReactProject/static/media/bg.e11f1346.jpg"
+  }
+]);
